0c4b599e05f77a329ce0f1c1237fef2428717dda
conectaviajes70-arch <conectaviajes70@gmail.com>
2026-05-06 04:16:15 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // js/config.js
 const CONFIG = {
-    APPS_SCRIPT_URL: "https://script.google.com/macros/s/TU_WEB_APP_URL_AQUI/exec", 
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwIeZQs-vmYt_hBYku6Y7SOHJrmQUpOyPKKe0fpQjk9E-C8QwighLqBNSLnBitJ2yO1/exec", 
     // ← Reemplaza con tu URL real del Apps Script
     
     GOOGLE_MAPS_KEY: "TU_CLAVE_GOOGLE_MAPS_AQUI", 
